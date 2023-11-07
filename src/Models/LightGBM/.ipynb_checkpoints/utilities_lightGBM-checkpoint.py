96cf549c88d001994f383d2a9311177470243bc1
Gustav Lokna <loknagustav@gmail.com>
2023-11-07 14:31:25 +0100

    fikset catboost og LighLGBM

--- src/Models/LightGBM/.ipynb_checkpoints/utilities_lightGBM-checkpoint.py
+++ src/Models/LightGBM/.ipynb_checkpoints/utilities_lightGBM-checkpoint.py
@@ -1,10 +1,8 @@
 from model_maker import *
-from catboost import CatBoostRegressor, Pool
+from lightgbm import LGBMRegressor
 
-class catboost_model(model):
+class lightGBM_model(model):
     def __init__(self, hyperparameters, X_train, X_test, y_train):
         self.X_test = X_test
         self.X_train, self.X_valid, self.y_train, self.y_valid = train_test_split(X_train, y_train, shuffle = True, random_state = 42)
-        self.X_train = X_train
-        self.y_train = y_train
-        self.model = CatBoostRegressor(**hyperparameters)
+        self.model = LGBMRegressor(**hyperparameters, random_state = 42)
