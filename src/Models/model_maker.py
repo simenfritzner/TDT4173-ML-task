23e1bcde9d6335c0ae12d1c2828c2094ae8856ab
Simen Fritzner <100536692+yadert@users.noreply.github.com>
2023-11-09 13:34:18 +0100

    Update model_maker.py

--- src/Models/model_maker.py
+++ src/Models/model_maker.py
@@ -17,8 +17,9 @@ class model():
     def pred(self):
         max_value = self.y_train["pv_measurement"].max()
         self.prediction = self.model.predict(self.X_test)
-        #self.pred_estimated = self.model.predict(self.X_valid)
+        self.pred_estimated = self.model.predict(self.X_valid)
         self.prediction = self.prediction.clip(min = 0, max = max_value)
+        self.predictions = self.floor_pred([self.prediction], [self.X_test])
     
     def feature_importence_plot(self):
         self.model.feature_importances_
@@ -56,79 +57,68 @@ class model():
         print("Cross-validation scores:", scores)
         print("Mean cross-validation score:", self.cross_val_score_mean)
         
-        
-    def cross_val_stack(self, X_train, y_train):
-        # Setup cross-validation scheme
-        kf = KFold(n_splits=5, shuffle=True, random_state=1)
-        
-        # Prepare to collect predictions and actual values
-        predictions = []
-        actuals = []
-        maes = []
-        # Iterate over each split
-        for train_index, test_index in kf.split(X_train):
-            # Split the data
-            X_train_fold, X_test_fold = X_train.iloc[train_index], X_train.iloc[test_index]
-            y_train_fold, y_test_fold = y_train.iloc[train_index], y_train.iloc[test_index]
-            
-            # Fit the model
-            self.model.fit(X_train_fold, y_train_fold["pv_measurement"])
-            
-            # Predict on the test fold
-            fold_predictions = self.model.predict(X_test_fold)
-            
-            fold_predictions = fold_predictions.clip(min = 0, max = self.y_train["pv_measurement"].max())
-            
-            # Calculate and store MAE for the fold
-            fold_mae = mean_absolute_error(y_test_fold["pv_measurement"], fold_predictions)
-            maes.append(fold_mae)
-            
-            # Store predictions and actual values
-            predictions.append(fold_predictions)
-            actuals.append(y_test_fold["pv_measurement"].values)
-        
-        self.cross_val_mae_mean = np.mean(maes)
-        # Concatenate to have full array of cross-validated predictions and actuals
-        self.cross_val_predictions = np.concatenate(predictions)
-        self.cross_val_actuals = np.concatenate(actuals)
-        return self.cross_val_predictions, self.cross_val_actuals, self.cross_val_mae_mean
     
-    def cross_val_stack_np(self, X_train, y_train):
-        # Setup cross-validation scheme
-        kf = KFold(n_splits=5, shuffle=True, random_state=1)
+    def cross_val_stack(self, X_train, y_train):
+            # Setup cross-validation scheme
+            kf = KFold(n_splits=5, shuffle=True, random_state=1)
+    
+            # Prepare to collect predictions, actual values, and DataFrames
+            predictions = []
+            actuals = []
+            maes = []
+            training_dfs = []  # List to collect training DataFrames
+    
+            # Iterate over each split
+            for train_index, test_index in kf.split(X_train):
+                # Split the data
+                X_train_fold, X_test_fold = X_train.iloc[train_index], X_train.iloc[test_index]
+                y_train_fold, y_test_fold = y_train.iloc[train_index], y_train.iloc[test_index]
+    
+                # Save the training DataFrame for this fold
+                training_dfs.append(X_test_fold)
+    
+                # Fit the model
+                self.model.fit(X_train_fold, y_train_fold["pv_measurement"])
+    
+                # Predict on the test fold
+                fold_predictions = self.model.predict(X_test_fold)
+    
+                fold_predictions = fold_predictions.clip(min=0, max=self.y_train["pv_measurement"].max())
+    
+                # Calculate and store MAE for the fold
+                fold_mae = mean_absolute_error(y_test_fold["pv_measurement"], fold_predictions)
+                maes.append(fold_mae)
+    
+                # Store predictions and actual values
+                predictions.append(fold_predictions)
+                actuals.append(y_test_fold["pv_measurement"].values)
+    
+            self.cross_val_mae_mean = np.mean(maes)
+            # Concatenate to have full array of cross-validated predictions and actuals
+            self.cross_val_predictions = np.concatenate(predictions)
+            self.cross_val_actuals = np.concatenate(actuals)
+            self.cross_val_floored = np.concatenate(self.floor_pred(predictions, training_dfs))
+            return self.cross_val_floored, self.cross_val_actuals, self.cross_val_mae_mean
         
-        # Prepare to collect predictions and actual values
-        predictions = []
-        actuals = []
-        maes = []
-        
-        # Iterate over each split
-        for train_index, test_index in kf.split(X_train):
-            # Split the data
-            X_train_fold, X_test_fold = X_train[train_index], X_train[test_index]
-            y_train_fold, y_test_fold = y_train.iloc[train_index], y_train.iloc[test_index]
-            
-            # Fit the model
-            self.model.fit(X_train_fold, y_train_fold["pv_measurement"])
-            
-            # Predict on the test fold
-            fold_predictions = self.model.predict(X_test_fold)
-            
-            fold_predictions = fold_predictions.clip(min = 0, max = self.y_train["pv_measurement"].max())
-            
-            # Calculate and store MAE for the fold
-            fold_mae = mean_absolute_error(y_test_fold["pv_measurement"], fold_predictions)
-            maes.append(fold_mae)
-            
-            # Store predictions and actual values
-            predictions.append(fold_predictions)
-            actuals.append(y_test_fold["pv_measurement"].values)
-        
-        self.cross_val_mae_mean = np.mean(maes)
-        
-        # Concatenate to have full array of cross-validated predictions and actuals
-        self.cross_val_predictions = np.concatenate(predictions)
-        self.cross_val_actuals = np.concatenate(actuals)
-        
-        return self.cross_val_predictions, self.cross_val_actuals, self.cross_val_mae_mean
-
+        def floor_pred(self, y_pred, X_valid):
+            floored_predictions = []
+            for i in range(len(y_pred)):
+                threshold = 0  # set your threshold here
+                predictions_fold = y_pred[i]
+                valid_df_fold = X_valid[i]
+    
+                # Make sure the indices align, this assumes `sun_elevation:d` is a column in the DataFrame
+                mask = valid_df_fold["sun_elevation:d"] < threshold
+    
+                # Check that mask and the predictions have the same length
+                if len(mask) != len(predictions_fold):
+                    raise ValueError(f"Length mismatch between mask and predictions in fold {i}: {len(mask)} vs {len(predictions_fold)}")
+    
+                # Apply the mask
+                predictions_fold = np.where(mask, 0, predictions_fold)
+    
+                # Append the floored predictions to the list
+                floored_predictions.append(predictions_fold)
+    
+            # At the end, concatenate all the floored prediction arrays
+            return floored_predictions
